16b4f332c1da33d98e602147ad41d78e4eb3dfd5
Federico <codepalace@fastmail.se>
2022-03-15 07:49:41 +0100

    Initial Commit

---
+++ MainProject/MainProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainProjectApp.swift
+//  MainProject
+//
+//  Created by Federico on 15/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MainProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
